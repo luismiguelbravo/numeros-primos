fb554fa037c166c45763e05a40a9d5babf12105e
Luis Miguel Bravo <lbravo@myhotel.com.es>
2022-04-08 19:32:49 -0400

    se agrega funcion para determinar los numeros primos en un intervalo entre 2 y el numero entregado, cuando el numero es menor a 2 se entrega un mensaje de error, cuando el parametro de entrada no es un numero se entrega un mensaje de error

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,40 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// En matemáticas, un número primo es un número natural mayor que 1 
+// que tiene únicamente dos divisores positivos distintos: él mismo y el 1.
+function esPrimo(numero) {
+  if (numero === 2 || numero === 3 ) {
+    return true;
+  }
+  for (let indice = 2; indice < numero; indice++) {
+    if (numero % indice === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+router.get('/numeros-primos', function(req, res, next) {
+  let numeroMaximo = req.query.maximo;
+  if(isNaN(numeroMaximo)){
+    res.status(500).json({ error: "El parámetro de entrada no es un número" });
+  } else {
+    if (numeroMaximo < 2 ) {
+      res.status(500).json({ error: "El número de entrada debe ser mayor o igual a 2" });
+    }
+    let arrayDePrimos = [];
+    let indice = 2;
+    while (indice <= numeroMaximo ) {
+      console.log(esPrimo);
+      if (esPrimo(indice)) {
+        arrayDePrimos[arrayDePrimos.length] = indice;
+      }
+      indice++;
+    }
+    res.json({ "arrayDePrimos": arrayDePrimos.reverse() });
+  }
+});
+
+
 module.exports = router;
